9ed0528a639853dca20df7f8c6567c960bc60474
MisteryPoints <misterypoint2@gmail.com>
2022-10-24 22:27:02 -0400

    Ending some features

--- pages/skills.js
+++ pages/skills.js
@@ -9,11 +9,11 @@ const GridSkill = styled.div`
     display: grid;
     grid-template-columns: repeat(2, minmax(0, 1fr));
     gap: 1.25rem; 
-    @media (min-width: 1280px) {
-        grid-template-columns: repeat(3, minmax(0, 1fr));
-    }
     @media (min-width: 480px) {
         grid-template-columns: repeat(4, minmax(0, 1fr));
+    }
+    @media (min-width: 1280px) {
+        grid-template-columns: repeat(3, minmax(0, 1fr));
     }
 `
 
